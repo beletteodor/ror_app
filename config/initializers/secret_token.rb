5de0417c0248711d27bf6ebca94924fae9e689c8
developer <developer@linuxmint.linuxmint-domain>
2013-08-09 11:56:15 -0400

    A little message that describes the changes we made

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RorApp::Application.config.secret_token = '93df65cf849c5e0b056344ac9b9c0d6a012b3a29e5891bfaba6a7fc26febb08ae6c04a6ae611bd9680697f067c838eca8881f56c06c46588ab2716fae8ab86f7'
